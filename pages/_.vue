51280ae52b3582060fe40399762173094caf39b2
Rafael Pedrosa <rafadpedrosa@gmail.com>
2021-10-20 15:56:16 -0300

    Add _.vue so redirect the user if he tries to access a not found page

---
+++ pages/_.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="text-center">
+      Welcome to My Nuxt Search!
+    </h1>
+    <hr>
+    <div class="page">
+      <NuxtLink class="btn btn-warning mr-3" to="/search">
+        Implementation
+      </NuxtLink>
+      <NuxtLink class="btn btn-warning" to="/technicalAssessmentDescription">
+        Exercise Description
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style>
+</style>
