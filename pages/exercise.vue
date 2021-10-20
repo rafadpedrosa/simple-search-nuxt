80838aa70d5e644724d24203fd001f092f7b4307
Rafael Pedrosa <rafael.pedrosa.br@avenuecode.com>
2021-10-20 11:03:53 -0300

    Update the index.vue and exercise.vue page with the new implementations

--- pages/exercise.vue
+++ pages/exercise.vue
@@ -1,7 +1,56 @@
 <template>
-  <Tutorial />
+  <div class="exercise-wrapper container">
+    <h1>Exercise</h1>
+
+    <SearchInput @input="setHighlightText" />
+
+    <PersonCard v-for="(user, i) in users" :key="i" class="mt-3" :user="user" :highlight="highlightText" />
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+import SearchInput from '@/components/SearchInput'
+import PersonCard from '@/components/PersonCard'
+
+export default {
+  components: {
+    SearchInput,
+    PersonCard
+  },
+  data () {
+    return {
+      highlightText: 'a',
+      usersData: null
+    }
+  },
+  watch: {
+    highlightText () {
+      this.userData = { ...this.users }
+    }
+  },
+  methods: {
+    setHighlightText (inputText) {
+      this.highlightText = inputText
+    }
+  },
+  computed: {
+    ...mapState(['users']),
+    persons () {
+      return this.userData || this.users
+    }
+  },
+  mounted () {
+    // getUsers
+  }
+}
 </script>
+
+<style type="text/css" lang="scss" scoped>
+.person-card {
+
+}
+
+.exercise-wrapper {
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    <Tutorial />
+  <div className="page">
+    <h1>Welcome to My Nuxt Search!</h1>
+    <NuxtLink to="/exercise">
+      Exercise Implementation
+    </NuxtLink>
+    <NuxtLink to="/technicalAssessmentDescription">
+      Exercise Description
+    </NuxtLink>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Index'
+}
 </script>
 
 <style>
-
 </style>
