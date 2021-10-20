aed3a269d0e8aae6535bb6dd99fb50f96fc33bf7
Rafael Pedrosa <rafael.pedrosa.br@avenuecode.com>
2021-10-20 12:02:57 -0300

    Create the technicalAssessmentDescription.vue page (to be implemented)

---
+++ pages/technicalAssessmentDescription.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1> :::TODO::: ADD the technical assessment description with a readable CSS</h1>
+    <!--    <h1>Frontend technical assessment</h1>-->
+    <!--    # **Bravado quest**-->
+
+    <!--    You are suggested to implement a simple profile search application using Nuxt and Vue frameworks. Expected-->
+    <!--    technologies used are: ES6, CSS (with any pre/post processors), html, SFC (Single File Component).-->
+
+    <!--    ## **Layout design**-->
+
+    <!--    Can be found <a-->
+    <!--      href="https://www.figma.com/file/PyncPYa1rpOxRooTdfvgxHRN/Bravado-Quest](https://www.figma.com/file/PyncPYa1rpOxRooTdfvgxHRN/Bravado-Quest"-->
+    <!--    >-->
+    <!--      here </a>-->
+
+    <!--    [https://camo.githubusercontent.com/c8c0b8694ebc33c77fa55814253962c4a289b027f420be492338094e247cbf36/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6272617661646f2d696d616765732d70726f64756374696f6e2f4465736b746f702e706e67](https://camo.githubusercontent.com/c8c0b8694ebc33c77fa55814253962c4a289b027f420be492338094e247cbf36/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6272617661646f2d696d616765732d70726f64756374696f6e2f4465736b746f702e706e67)-->
+
+    <!--    ## **Expected steps**-->
+
+    <!--    You are suggested to implement layout according to design above and a single page application based on Vue and its-->
+    <!--    modules. Application should be running on a root url (`/`). The sample data to use inside application should be-->
+    <!--    taken <a-->
+    <!--      href="https://gist.github.com/allaud/093aa499998b7843bb10b44ea6ea02dc](https://www.notion.so/093aa499998b7843bb10b44ea6ea02dc"-->
+    <!--    >here</a>-->
+
+    <!--    Please note, the default app state contains all profile cards. When user starts typing, the results should be-->
+    <!--    filtered in realtime, highlighting the matching string.-->
+
+    <!--    ## **The task highlights**-->
+
+    <!--    - General SPA application performance, including searching, scrolling and highlighting speed.-->
+    <!--    - Quality of Vue single file components (SFC)-->
+    <!--    - HTML(JSX) and CSS quality-->
+    <!--    - JS code quality-->
+    <!--    - Search should be stateful (using browser url e.g.`/search/Boris`)-->
+    <!--    - Application should be published online for testing purposes-->
+    <!--    <Tutorial />-->
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
