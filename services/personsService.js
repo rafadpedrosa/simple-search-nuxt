0d34c0bda37eb85493cbbdccc0a29d97e0431048
Rafael Pedrosa <rafael.pedrosa.br@avenuecode.com>
2021-10-20 12:00:20 -0300

    Update point the personsService.js to the correct endpoint (users.json)

--- services/personsService.js
+++ services/personsService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default {
   getUsers () {
-    return axios.get('http://localhost:3000/users-small.json')
-    // return axios.get('http://localhost:3000/users.json')
+    // return axios.get('http://localhost:3000/users-small.json')
+    return axios.get('http://localhost:3000/users.json')
   }
 }
