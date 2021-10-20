37a7cc7a85ef26bd72d1a68e72e62c496def94df
Rafael Pedrosa <rafadpedrosa@gmail.com>
2021-10-20 15:50:30 -0300

    Improve the directive to highlight when the component is inserted as well

--- directives/global.js
+++ directives/global.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 
-Vue.directive('highlight', {
-  componentUpdated: (el, { value }) => {
-    // eslint-disable-next-line prefer-regex-literals
-    const regExpCleaner = new RegExp('(<span style="background:yellow">|</span>)', 'g')
+const update = (el, { value }) => {
+  // eslint-disable-next-line prefer-regex-literals
+  const regExpCleaner = new RegExp('(<span style="background:yellow">|</span>)', 'g')
 
-    el.innerHTML = el.innerHTML.replaceAll(regExpCleaner, '')
-    el.innerHTML = el.innerHTML.replaceAll(value.highlight, `<span style="background:yellow">${value.highlight}</span>`)
-  }
+  el.innerHTML = el.innerHTML.replaceAll(regExpCleaner, '')
+  el.innerHTML = el.innerHTML.replaceAll(value.highlight, `<span style="background:yellow">${value.highlight}</span>`)
+}
+
+Vue.directive('highlight', {
+  inserted: update,
+  componentUpdated: update
 })
