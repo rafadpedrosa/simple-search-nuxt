d84342c2aaaf99924515d1004df108fa06d0419e
Rafael Pedrosa <rafael.pedrosa.br@avenuecode.com>
2021-10-20 10:47:44 -0300

    create directive highlight

--- directives/global.js
+++ directives/global.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 
-Vue.directive('focus', {
-  inserted: (el) => {
-    debugger
-    console.log(el.style)
+Vue.directive('highlight', {
+  componentUpdated: (el, { value }) => {
+    // eslint-disable-next-line prefer-regex-literals
+    const regExpCleaner = new RegExp('(<span style="background:yellow">|</span>)', 'g')
+
+    el.innerHTML = el.innerHTML.replaceAll(regExpCleaner, '')
+    el.innerHTML = el.innerHTML.replaceAll(value.highlight, `<span style="background:yellow">${value.highlight}</span>`)
   }
 })
