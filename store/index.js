caaa763ae2821258f221e033df31ed02b02e5291
Rafael Pedrosa <rafadpedrosa@gmail.com>
2021-10-20 15:53:35 -0300

    Remove nuxtServerInit; Improve the store adding the filteredUsers with the byName var; Fix the the lastPage search

--- store/index.js
+++ store/index.js
@@ -1,59 +1,64 @@
-import { SET_PAGE, UPDATE_PAGINATED_USERS, SET_USERS } from '@/consts/mutations'
-import personsService from '@/services/personsService'
+import { SET_PAGE, UPDATE_PAGINATED_USERS, SET_USERS, CLEAR_PAGINATED_USERS, SET_FILTER } from '@/consts/mutations'
 import { UPDATE_PAGINATION } from '@/consts/actions'
 
 export const state = () => ({
   users: [],
+  usersFiltered: [],
   usersPaginated: [],
+  byName: null,
   pagination: {
-    currentPage: 0,
+    currentPage: 1,
     offset: 0,
     size: 10
   }
 })
 
 export const getters = {
-  usersSize: state => state.users.length,
+  usersSize: state => state.byName ? state.usersFiltered.length : state.users.length,
   perPage: state => state.pagination.size,
   currentPage: state => state.pagination.currentPage
 }
 
 export const mutations = {
+  [SET_FILTER]: (state, byName) => {
+    state.byName = byName
+  },
   [SET_PAGE]: (state, currentPage) => {
     state.pagination.currentPage = currentPage
 
-    const isLastPage = state.pagination.size > state.users.length
+    const searchSize = state.pagination.size + state.pagination.offset
+    const isLastPage = searchSize > state.users.length
 
     if (!isLastPage) {
       state.pagination.offset = (state.pagination.currentPage * state.pagination.size) - state.pagination.size
     }
   },
+  [CLEAR_PAGINATED_USERS]: (state) => {
+    state.usersPaginated = []
+  },
   [UPDATE_PAGINATED_USERS]: (state) => {
     state.usersPaginated = []
 
-    const isLastPage = state.pagination.size > state.users.length
-    const size = isLastPage ? state.users.length : state.pagination.size + state.pagination.offset
+    const searchSize = state.pagination.size + state.pagination.offset
+    const users = state.byName ? state.usersFiltered : state.users
+    const isLastPage = searchSize > users.length
+    const size = isLastPage ? users.length : searchSize
 
     for (let i = state.pagination.offset; i < size; i++) {
-      state.usersPaginated.push(state.users[i])
+      state.usersPaginated.push(users[i])
     }
   },
   [SET_USERS]: (state, users) => {
     state.users = users
+    state.usersFiltered = users.filter(user => user.name.includes(state.byName))
   }
 }
 
 export const actions = {
-  async nuxtServerInit ({ commit, state }) {
-    // TODO add error handling
-    const response = await personsService.getUsers()
-    const users = response.data
-
-    commit(SET_USERS, users)
-    commit(UPDATE_PAGINATED_USERS)
-  },
   [UPDATE_PAGINATION] ({ commit, state }, currentPage) {
-    commit(SET_PAGE, currentPage)
-    commit(UPDATE_PAGINATED_USERS)
+    return Promise.resolve().then(() => {
+      commit(SET_PAGE, currentPage)
+      commit(UPDATE_PAGINATED_USERS)
+    })
   }
 }
