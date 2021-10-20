1adc6013fd31d8690711486d5ab3f225bf4aaf80
Rafael Pedrosa <rafael.pedrosa.br@avenuecode.com>
2021-10-20 08:52:59 -0300

    Add global.css and scss support

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/global.css',
+    '@/assets/scss/main.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -44,6 +46,10 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios'
   ],
+
+  bootstrapVue: {
+    icons: true
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
